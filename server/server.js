18b78131c1c6a6632382dcd99fef988b685d852b
Shane Moyo <smoyo@cvent.com>
2019-04-09 15:59:09 -0700

    Add server.js

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const app = require('./lib/app');
+const connect = require('./lib/connect');
+
+connect(process.env.MONGODB_URI);
+const port = process.env.PORT;
+
+app.listen(port, () => console.log('Server started, port: ', port));
