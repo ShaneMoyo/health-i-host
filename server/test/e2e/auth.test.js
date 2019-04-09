771edeb0dcdf03445f455d15e7db897e8f085402
Shane Moyo <smoyo@cvent.com>
2019-04-09 15:03:50 -0700

    Add Auth e2e test and utils

---
+++ server/test/e2e/auth.test.js
@@ -0,0 +1,64 @@
+const request = require('./request');
+const assert = require('chai').assert;
+const mongoose = require('mongoose');
+const User = require('../../lib/models/user');
+const tokenService = require('../../lib/utils/token-service');
+
+
+describe('Auth API', () => {
+  beforeEach(() => mongoose.connection.dropDatabase());
+
+  it('Should generate a token on signup', () => {
+    return request.post('/api/auth')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      })
+      .then(({ body: token }) => assert.ok(token))
+  });
+
+  it('throws error if email already exists',() => {
+  return request.post('/api/auth')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .then(()=> {
+      return request.post('/api/auth')
+        .send({
+          email: 'test@test.com',
+          password: 'password'
+        })
+        .then((err) => assert.equal(err.status, 400));
+    });
+  });
+
+  it('Should throw an error if password is not included', () => {
+    return request.post('/api/auth')
+        .send({
+            email: 'test2@test.com',
+            password: ''
+        })
+        .then(err => assert.equal(err.status, 400));
+  });
+
+  it('Should signin with valid credentials', () => {
+    return request.post('/api/auth')
+    .send({
+        email: 'test@test.com',
+        password: 'password'
+    })
+    .then(()=> {
+      return request
+        .post('/api/auth/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .then(({ body: token }) => {
+            assert.equal(typeof token, 'string');
+        });
+    });
+  });
+
+})
