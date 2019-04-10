4b8e38ade01f69ad345b413ef2226b36c41c753f
Shane Moyo <smoyo@cvent.com>
2019-04-10 09:04:14 -0700

    Default seever port to 3001

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ const app = require('./lib/app');
 const connect = require('./lib/connect');
 
 connect(process.env.MONGODB_URI);
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log('Server started, port: ', port));
