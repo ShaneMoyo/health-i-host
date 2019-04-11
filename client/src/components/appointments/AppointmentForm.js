389eaa9168cd54c366a9efafd3ac043d2719e31b
Shane Moyo <smoyo@cvent.com>
2019-04-11 10:24:10 -0700

    add new date picker

--- client/src/components/appointments/AppointmentForm.js
+++ client/src/components/appointments/AppointmentForm.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
-import DateTimePicker from 'react-datetime-picker';
+import moment from 'moment'
+import Select from 'react-select'
+import DayPicker from 'react-day-picker';
+import TimePicker from 'rc-time-picker';
+
+import 'react-day-picker/lib/style.css';
+import 'rc-time-picker/assets/index.css';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+
+const format = 'h:mm a';
+
+const now = moment().hour(0).minute(0);
+
+
 
 export default class MyApp extends Component {
   state = {
-    date: new Date(),
+    date: new Date()
+
   }
 
   onChange = date => {
@@ -14,10 +33,33 @@ export default class MyApp extends Component {
   render() {
     return (
       <section class="hero is-warning is-fullheight">
-        <DateTimePicker
-          onChange={this.onChange}
-          value={this.state.date}
-        />
+        <div class="column is-waring is-6 is-offset-3">
+          <div class="box animated fadeIn is-warning" >
+            <div class="field">
+              <label class="label">Service</label>
+              <Select options={options} />
+            </div>
+            <hr/>
+            <div class="field">
+              <label class="label">Date</label>
+              <DayPicker />
+            </div>
+            <hr/>
+
+            <div class="field">
+              <label class="label">Time</label>
+              <TimePicker
+                 showSecond={false}
+                 defaultValue={now}
+                 className="xxx"
+                 onChange={this.onChange}
+                 format={format}
+                 use12Hours
+                 inputReadOnly
+               />
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
