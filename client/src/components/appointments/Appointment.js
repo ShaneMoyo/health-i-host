962cf52286edc1b50348c4cc9174f072caefe1f5
Shane Moyo <smoyo@cvent.com>
2019-04-11 11:05:36 -0700

    wip

--- client/src/components/appointments/Appointment.js
+++ client/src/components/appointments/Appointment.js
@@ -7,12 +7,6 @@ class Appointments extends Component {
       <section class="hero is-warning">
         <div class="hero-body">
           <div class="container has-text-centered">
-            <br/>
-            <br/>
-            <h3 class="animated fadeIn title">Book an Appointment</h3>
-            <p class="animated fadeIn subtitle">Please fill out the required fields to proceed</p>
-            <hr/>
-            <br/>
             <div class="container has-text-centered">
               <AppointmentForm/>
             </div>
--- client/src/components/appointments/AppointmentForm.js
+++ client/src/components/appointments/AppointmentForm.js
@@ -7,57 +7,79 @@ import TimePicker from 'rc-time-picker';
 import 'react-day-picker/lib/style.css';
 import 'rc-time-picker/assets/index.css';
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-]
-
-const format = 'h:mm a';
-
-const now = moment().hour(0).minute(0);
-
-
-
 export default class MyApp extends Component {
   state = {
-    date: new Date()
 
   }
 
-  onChange = date => {
-    console.log("date", date);
-    this.setState({ date })
+  getDate = () => {
+    const now = moment().hour(0).minute(0);
+    const { month, day, timeHour, timeMinutes } = this.state;
+    if (!month || !day || !timeHour || !timeMinutes) {
+      return now;
+    } else {
+      const newDate = new Date(2019, month, day, timeHour, timeMinutes);
+      console.log('returning ', newDate);
+      return newDate.toDateString()
+    }
+  }
+
+  onDateChange = date => {
+    if (!date) return
+    this.setState({ month: date.getMonth(), day: date.getDate() })
+  }
+
+  onTimeChange = date => {
+    if (!date) return
+    this.setState({ timeMinutes: date.format("mm"), timeHour: date.format("hh"), ampm: date.format("a")})
+  }
+
+  handleSubmit = () => {
+
+    console.log('appointment', this.getDate().toDateString())
   }
 
   render() {
+    const now = moment().hour(0).minute(0);
+    const selectedDates = this.getDate();
+    const options = [
+      { value: 'massage', label: 'Massage Therapy' },
+      { value: 'mineral', label: 'Mineral Consultation' },
+      { value: 'movement', label: 'Movement' }
+    ];
+
     return (
       <section class="hero is-warning is-fullheight">
         <div class="column is-waring is-6 is-offset-3">
           <div class="box animated fadeIn is-warning" >
-            <div class="field">
-              <label class="label">Service</label>
-              <Select options={options} />
-            </div>
-            <hr/>
-            <div class="field">
-              <label class="label">Date</label>
-              <DayPicker />
-            </div>
-            <hr/>
+            <div class="animated fadeIn title is-4">Schedule an Appointment</div>
 
             <div class="field">
-              <label class="label">Time</label>
+              <Select defaultValue="select a service" options={options} />
+            </div>
+            <hr/>
+            <div class="field">
+
+              <DayPicker onDayClick={this.onDateChange}/>
               <TimePicker
                  showSecond={false}
                  defaultValue={now}
                  className="xxx"
-                 onChange={this.onChange}
-                 format={format}
+                 onChange={this.onTimeChange}
+                 format='h:mm a'
                  use12Hours
                  inputReadOnly
                />
             </div>
+            <hr/>
+            <div class="field">
+              <label class="label"></label>
+              <button class="button is-medium is-info" onClick={this.handleSubmit}>Book Appointment</button>
+            </div>
+
+            <br/>
+
+            <br/>
           </div>
         </div>
       </section>
