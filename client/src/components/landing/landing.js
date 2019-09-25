cff36a1b0e3d5db0db42fc29f6d53eb120fdeca8
Shane Moyo <smoyo@cvent.com>
2019-09-25 14:21:29 -0700

    more landing page clean up

--- client/src/components/landing/landing.js
+++ client/src/components/landing/landing.js
@@ -18,6 +18,8 @@ export default class Landing extends Component {
         <section class="animated fadeIn hero is-info is-fullheight">
           <div class="hero-body">
             <div class="column is-6 is-offset-3 has-text-centered">
+              <br/><br/><br/>
+              <br/><br/><br/>
               <div class="tile is-parent">
                 <article class="tile is-child notification is-success landing">
                   <div class="content">
@@ -29,8 +31,6 @@ export default class Landing extends Component {
               <br/><br/><br/>
               <br/><br/><br/>
               <br/><br/><br/>
-              <br/><br/><br/>
-              <br/><br/><br/>
               <br/>
             </div>
           </div>
@@ -38,10 +38,12 @@ export default class Landing extends Component {
         <div class="box is-white">
           <br/><br/><br/>
           <br/><br/><br/>
+
             <Fade >
               <div class="tile is-ancestor">
                 <div class="tile is-vertical">
                   <div class="tile">
+
                     <NavBarLink exact to="/massage">
                       <article class="tile is-child notification is-primary grow">
                         <span class="icon is-large is-pulled-left" >
@@ -56,68 +58,61 @@ export default class Landing extends Component {
                       </article>
                     </NavBarLink>
 
-              <NavBarLink exact to="/minerals">
-                <article class="tile is-child notification is-warning has-text-white grow">
-                <span class="icon is-large is-pulled-left" >
-                  <Icon path={mdiGrain}
-                  size={2}
-                  color="white"
-                  />
-                </span>
-                  <p class="title">Mineral Therapy</p>
-                  <br/>
-                  <p class="subtitle">Learn More</p>
-                </article>
+                    <NavBarLink exact to="/minerals">
+                      <article class="tile is-child notification is-warning has-text-white grow">
+                        <span class="icon is-large is-pulled-left" >
+                          <Icon path={mdiGrain}
+                          size={2}
+                          color="white"
+                          />
+                        </span>
+                        <p class="title">Mineral Therapy</p>
+                        <br/>
+                        <p class="subtitle">Learn More</p>
+                      </article>
+                    </NavBarLink>
 
-              </NavBarLink>
+                    <NavBarLink exact to="/movement">
+                      <article class="tile is-child notification is-success grow">
+                        <span class="icon is-large is-pulled-left" >
+                          <Icon path={mdiRun}
+                          size={2}
+                          color="white"
+                          />
+                        </span>
+                        <p class="title">Movement Therapy</p>
+                        <br/>
+                        <p class="subtitle">Learn More</p>
+                      </article>
+                    </NavBarLink>
 
-              <NavBarLink exact to="/movement">
-
-              <article class="tile is-child notification is-success grow">
-              <span class="icon is-large is-pulled-left" >
-                <Icon path={mdiRun}
-                size={2}
-                color="white"
-                />
-              </span>
-                <p class="title">Movement Therapy</p>
+                  </div>
+                </div>
+              </div>
+            </Fade>
+            <br/>
+            <br/>
+            <Fade>
+              <div class="container has-text-centered">
+                <div class="subtitle has-text-centered">
+                  A healthy body allows us to live an optimal life with energy and enthusiasm.
+                  Do you want to be in balance, free from pain, able to be active, and doing your heart’s desires?
+                </div>
                 <br/>
-                <p class="subtitle">Learn More</p>
-              </article>
+                <div class="subtitle has-text-centered">
+                  <NavBarLink exact to="/appointment">
+                    <div class="tile is-parent is-vertical grow more">
+                      <article class="tile is-child notification has-text-white is-info">
+                      <p class="title">
+                        Schedule Appointment
+                      </p>
+                      </article>
+                    </div>
+                  </NavBarLink>
+                </div>
+              </div>
+            </Fade>
 
-              </NavBarLink>
-
-
-            </div>
-
-          </div>
-
-        </div>
-        </Fade>
-        <br/>
-        <br/>
-        <Fade>
-        <div class="container has-text-centered">
-          <div class="subtitle has-text-centered">
-            A healthy body allows us to live an optimal life with energy and enthusiasm.
-            Do you want to be in balance, free from pain, able to be active, and doing your heart’s desires?
-
-          </div>
-          <br/>
-          <div class="subtitle has-text-centered">
-          <NavBarLink exact to="/appointment">
-            <div class="tile is-parent is-vertical grow more">
-              <article class="tile is-child notification has-text-white is-info">
-              <p class="title">
-                Schedule Appointment
-              </p>
-              </article>
-            </div>
-            </NavBarLink>
-          </div>
-        </div>
-
-        </Fade>
         </div>
       </body>
     );
